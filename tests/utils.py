063854afd6b578251188f12470c9fe74ae0be3bd
Noah <49675916+66Noah2018@users.noreply.github.com>
2024-02-13 12:32:51 +0100

    Update utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -1,2 +1,23 @@
 def normalize(org_string):
     return "".join(org_string.split())
+
+def getDefaultRangeCode(default_range):
+    return '''
+    id: default_range_tests
+    name: default_range_tests
+    title: Default range tests
+    prefixes:
+        linkml: https://w3id.org/linkml
+    default_range: {default_range}
+                          
+    classes:
+        default_range_tests:
+            name: default_range_tests
+            slots:
+                - default_range_slot
+    slots:
+        default_range_slot:
+            name: default_range_slot
+            title: Default range slot
+            description: Default range slot description
+'''.format(default_range=default_range)
