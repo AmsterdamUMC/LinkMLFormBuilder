6b098d931bcb1cc831e03f719e8ca1dfafa5843e
Noah <49675916+66Noah2018@users.noreply.github.com>
2024-02-19 09:43:02 +0100

    Update buildform_test.py

--- tests/buildform_test.py
+++ tests/buildform_test.py
@@ -12,19 +12,15 @@ def test_three_zibs_form(yaml_3ZIB_complex, three_zibs_form_expected):
     assert normalize(buildForm(yaml_3ZIB_complex, True, "cwd", "default", True)) == normalize(three_zibs_form_expected)
     os.remove(os.path.join(os.getcwd(), "3Zorginformatiebouwstenen_(ZIB)_2020.html"))
 
-# the tests below require the user to manually delete the HTML files: personinfo.html and personinfo5.html. This is due to the file not being closed when errors are thrown
-
 def test_tutorial1_yaml(tutorial1_yaml, tutorial1_yaml_expected):
-    with pytest.raises(TypeError) as excinfo:
-        buildForm(tutorial1_yaml, True, "cwd", "default", True)    
-    assert tutorial1_yaml_expected in str(excinfo.value)
-    
+    assert normalize(buildForm(tutorial1_yaml, True, "cwd", "default", True)) == normalize(tutorial1_yaml_expected)
+    os.remove(os.path.join(os.getcwd(), "personinfo.html"))
+
 def test_tutorial1_no_name_yaml(tutorial1_no_name_yaml, tutorial1_no_name_yaml_expected):
-    with pytest.raises(TypeError) as excinfo:
-        buildForm(tutorial1_no_name_yaml, True, "cwd", "default", True)    
-    assert tutorial1_no_name_yaml_expected in str(excinfo.value)
+    assert normalize(buildForm(tutorial1_no_name_yaml, True, "cwd", "default", True)) == normalize(tutorial1_no_name_yaml_expected)
+    os.remove(os.path.join(os.getcwd(), "formDefault.html"))
 
 def test_tutorial5_yaml(tutorial5_yaml, tutorial5_yaml_expected):
-    with pytest.raises(TypeError) as excinfo:
-        buildForm(tutorial5_yaml, True, "cwd", "default", True)    
-    assert tutorial5_yaml_expected in str(excinfo.value)
+    assert normalize(buildForm(tutorial5_yaml, True, "cwd", "default", True)) == normalize(tutorial5_yaml_expected)
+    os.remove(os.path.join(os.getcwd(), "personinfo5.html"))
+    
